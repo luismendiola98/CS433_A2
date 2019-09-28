47b121f65728c516b9052e22e1b0a37ed3e0797f
luismendiola98 <47489017+luismendiola98@users.noreply.github.com>
2019-09-28 14:43:53 -0700

    Delete .simple.ko.cmd

--- .simple.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/osc/mendi016/hw1/simple.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/osc/mendi016/hw1/simple.ko /home/osc/mendi016/hw1/simple.o /home/osc/mendi016/hw1/simple.mod.o
